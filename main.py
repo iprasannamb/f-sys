e6bed62ee888d87f10dd2355569986fb568707ba
Prasanna <iprasannamb@gmail.com>
2025-12-24 22:01:04 +0530

    feat: Implement interactive menu for file operations in filesystem

--- main.py
+++ main.py
@@ -2,8 +2,22 @@ from filesystem import FileSystem
 
 fs = FileSystem()
 
-fs.create_file("file1.txt")
-fs.write_file("file1.txt", "This is an inode based file system demo")
-print(fs.read_file("file1.txt"))
-
-fs.show_inode("file1.txt")
+while(1):
+    print("\n 1. Create File\n 2. Write File\n 3. Read File\n 4. Show Inode\n5.Exit");
+    opt=int(input("Enter you choice :"))
+    match(opt):
+        case 1:
+            name=input("Enter file name :")
+            fs.create_file("file1.txt")
+            
+        case 2:
+            fs.write_file("file1.txt","This is an inode based file system demo")
+            
+        case 3:
+            print(fs.read_file("file1.txt"))
+            
+        case 4:
+            fs.show_inode("file1.txt")
+            
+        case 5:
+            exit(0);
