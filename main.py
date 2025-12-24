5c817ff2abfa831ecd2316031b053c8c28a56878
Prasanna <iprasannamb@gmail.com>
2025-12-24 22:05:58 +0530

    refactor: File operation handling

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ from filesystem import FileSystem
 fs = FileSystem()
 
 while(1):
-    print("\n 1. Create File\n 2. Write File\n 3. Read File\n 4. Show Inode\n5.Exit");
+ 
+    print("\n 1. Create File\n 2. Write File\n 3. Read File\n 4. Show Inode \n 5.Exit");
     opt=int(input("Enter you choice :"))
     match(opt):
         case 1:
-            name=input("Enter file name :")
-            fs.create_file("file1.txt")
+            fname=input("Enter file name :")
+            fs.create_file(fname)
             
         case 2:
-            fs.write_file("file1.txt","This is an inode based file system demo")
+            fcontent=input("Enter the content to store :")
+            fs.write_file(f"{fname}",f"{fcontent}")
             
         case 3:
-            print(fs.read_file("file1.txt"))
+            print(fs.read_file(fname))
             
         case 4:
-            fs.show_inode("file1.txt")
+            fs.show_inode(fname)
             
         case 5:
             exit(0);
